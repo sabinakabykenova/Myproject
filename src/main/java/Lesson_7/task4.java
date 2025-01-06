04b3988ac943b9f26bd3ce98e38a1041157bc490
Sabina <kabykenova2002@gmail.com>
2025-01-06 21:23:09 +0500

    completed

---
+++ src/main/java/Lesson_7/task4.java
@@ -0,0 +1,9 @@
+package Lesson_7;
+
+public class task4 {
+    public static void printStringNTimes(String str, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(str);
+        }
+    }
+}
