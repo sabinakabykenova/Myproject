dcd56ce6a238b32475c707dab97fed758cb2cefa
Sabina <kabykenova2002@gmail.com>
2025-01-09 21:56:48 +0500

    completed

---
+++ src/main/java/Lesson_8/Employee.java
@@ -0,0 +1,31 @@
+package Lesson_8;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    public int age;
+
+    // Инициализацие полей
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    // Вывод информации о сотруднике
+    public void printInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+        System.out.println();
+    }
+}
